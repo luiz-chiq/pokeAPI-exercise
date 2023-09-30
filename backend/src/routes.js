78fecf102c6776b45adfe6983dd9d9c4477294a4
luiz-chiq <chiquetanoluiz@gmail.com>
2023-09-30 16:25:13 -0300

    feat: criação da rota de signup

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,4 +2,23 @@ const express = require('express');
 
 const routes = express.Router();
 
+const requireAuth = require('./middleware/requireAuth');
+
+const users = [];
+
+routes.post('/signup', (req, res) => {
+        const newUser = req.body;
+      
+        if (!newUser || !newUser.name || !newUser.email || !newUser.password) {
+          return res.status(400).json({ error: 'Dados do usuário inválidos' });
+        }
+        newUser.id = users.length + 1;
+        newUser.pokemons = [];
+        users.push(newUser);
+      
+        res.status(201).json(newUser);
+});
+
+
+
 module.exports = routes;
